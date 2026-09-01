1fd1695754d67412f36ba8738a4fe2ad5d66d588
EvKD <kaushik.evyavan@students.kisunu.braeburn.ac.ke>
2026-09-01 19:59:13 +0300

    TEN

---
+++ Loops_1/ten10.java
@@ -0,0 +1,8 @@
+public class ten10 {
+    public static void main(String[] args) {
+        for (int i = 15; i >= 5; i--) {
+            System.out.println("The value of i is:" + i);
+        }
+            
+    }
+}
