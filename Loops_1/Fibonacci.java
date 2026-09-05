df6f713881bd43e422bab73fc8549b78389bf7f3
EvKD <kaushik.evyavan@students.kisunu.braeburn.ac.ke>
2026-09-05 14:54:04 +0300

    FIBONACCI

---
+++ Loops_1/Fibonacci.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of Fibonacci numbers: ");
+        int n = sc.nextInt();
+
+        int first = 0;
+        int second = 1;
+
+        System.out.println("First " + n + " Fibonacci numbers:");
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(first + " ");
+
+            int next = first + second;
+            first = second;
+            second = next;
+        }
+
+        sc.close();
+    }
+}
