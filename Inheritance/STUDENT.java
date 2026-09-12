e40abf10b4db309b05788bbde62651f80bd15941
EvKD <kaushik.evyavan@students.kisunu.braeburn.ac.ke>
2026-09-12 14:39:46 +0300

    bello

---
+++ Inheritance/STUDENT.java
@@ -0,0 +1,25 @@
+class Parent {
+    int age, id;
+    String name;
+
+    void naming(String name) {
+        System.out.println("My name is " + name);
+    }
+} 
+
+class Child extends Parent {
+    void ageN(int age) {
+        System.out.println("My age is " + age);
+
+        } 
+    }
+
+
+
+    class student {
+        public static void main(String[] args) {
+            Child c = new Child();
+            c.naming("Evyavan Kaushik");
+            c.ageN(20);
+        }
+    }
